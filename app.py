a788b12fafde67881ee0d13d4230b5ed4cbb0249
NGV <95875573+ngv-sama@users.noreply.github.com>
2023-10-15 00:12:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,49 +19,92 @@ df = pd.read_csv(StringIO(data))
 # Streamlit app
 st.title("Personality Traits Analysis")
 
-# Dropdown menu to select team member
-selected_team_member = st.selectbox("Select Team Member:", df["Team Members"])
+# Divide the page into two columns
+col1, col2 = st.beta_columns(2)
 
-# Filter data based on the selected team member
-selected_data = df[df["Team Members"] == selected_team_member].squeeze()
+# Left column
+with col1:
+    st.header("Trait Explanations")
 
-# Create a bar chart using Plotly
-fig = go.Figure()
+    trait_descriptions = {
+        "EXTROVERTED": "😄 Extroverted individuals are outgoing, energetic, and enthusiastic. They enjoy social interactions and thrive in group settings.",
+        "INTROVERTED": "🤫 Introverted individuals are reserved, quiet, and thoughtful. They prefer solitary activities and find social interactions draining.",
+        "INTUITIVE": "🧠 Intuitive individuals rely on gut feelings, instincts, and imagination. They focus on possibilities and the future.",
+        "OBSERVANT": "👀 Observant individuals notice and focus on physical details. They are attentive to their surroundings and notice small changes.",
+        "THINKING": "🤔 Thinking individuals make decisions based on logic, analysis, and reason. They are objective and value fairness in decision-making.",
+        "FEELING": "❤️ Feeling individuals make decisions based on emotions, values, and personal beliefs. They consider the impact on others and seek harmony.",
+        "JUDGING": "🗂️ Judging individuals are organized, structured, and decisive. They prefer clear plans and order in their lives.",
+        "PROSPECTING": "🔄 Prospecting individuals are adaptable, spontaneous, and flexible. They enjoy new experiences and prefer to go with the flow.",
+        "ASSERTIVE": "💪 Assertive individuals are confident, self-assured, and proactive. They are comfortable taking charge and making decisions.",
+        "TURBULENT": "😰 Turbulent individuals are emotionally sensitive, self-conscious, and prone to self-doubt. They may experience higher levels of stress and anxiety."
+    }
 
-for i, trait in enumerate(selected_data.index[1:]):
-    fig.add_trace(go.Bar(
-        x=[trait],
-        y=[selected_data[trait]],
-        name=trait,
-        marker=dict(color=f'rgba({i * 30}, 50, 190, 0.7)'),  # Adjust color
-        text=[f'{selected_data[trait]}'],
-        textposition='auto',
+    for trait, description in trait_descriptions.items():
+        st.write(f"**{trait}** : {description}")
+
+    st.header("Trait Belonging")
+
+    selected_team_member = st.selectbox("Select Team Member:", df["Team Members"])
+    selected_data = df[df["Team Members"] == selected_team_member].squeeze()
+
+    for trait in selected_data.index[1:]:
+        if selected_data[trait] > 50:
+            st.write(f"{selected_team_member} belongs to the trait: {trait}")
+
+    st.header("Pairwise Comparisons")
+    trait_pairs = st.multiselect("Select Traits for Comparison:", df.columns[1:])
+    if trait_pairs:
+        st.write(f"You selected: {trait_pairs}")
+
+    st.header("Team Trait Distribution")
+
+    # Create a data frame for trait distribution
+    trait_distribution = df.drop("Team Members", axis=1).gt(50).sum()
+
+    # Create a ring chart
+    fig = go.Figure(go.Pie(
+        labels=trait_distribution.index,
+        values=trait_distribution.values,
+        hole=0.4,
+        pull=[0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
     ))
 
-fig.update_layout(
-    title=f"Personality Traits for {selected_team_member}",
-    xaxis=dict(title="Traits"),
-    yaxis=dict(title="Values"),
-    barmode='group'
-)
+    # Display the ring chart
+    st.plotly_chart(fig)
 
-# Display brief descriptions of each personality trait
-trait_descriptions = {
-    "EXTROVERTED": "😄 Extroverted individuals are outgoing, energetic, and enthusiastic. They enjoy social interactions and thrive in group settings.",
-    "INTROVERTED": "🤫 Introverted individuals are reserved, quiet, and thoughtful. They prefer solitary activities and find social interactions draining.",
-    "INTUITIVE": "🧠 Intuitive individuals rely on gut feelings, instincts, and imagination. They focus on possibilities and the future.",
-    "OBSERVANT": "👀 Observant individuals notice and focus on physical details. They are attentive to their surroundings and notice small changes.",
-    "THINKING": "🤔 Thinking individuals make decisions based on logic, analysis, and reason. They are objective and value fairness in decision-making.",
-    "FEELING": "❤️ Feeling individuals make decisions based on emotions, values, and personal beliefs. They consider the impact on others and seek harmony.",
-    "JUDGING": "🗂️ Judging individuals are organized, structured, and decisive. They prefer clear plans and order in their lives.",
-    "PROSPECTING": "🔄 Prospecting individuals are adaptable, spontaneous, and flexible. They enjoy new experiences and prefer to go with the flow.",
-    "ASSERTIVE": "💪 Assertive individuals are confident, self-assured, and proactive. They are comfortable taking charge and making decisions.",
-    "TURBULENT": "😰 Turbulent individuals are emotionally sensitive, self-conscious, and prone to self-doubt. They may experience higher levels of stress and anxiety."
-}
+    st.header("Team Members for Each Trait")
+    for trait in trait_distribution.index:
+        trait_members = df[df[trait] > 50]["Team Members"].tolist()
+        st.write(f"**{trait}**: {', '.join(trait_members)}")
 
-st.plotly_chart(fig)
+# Right column
+with col2:
+    st.header("Personality Traits Analysis")
 
-# Display trait descriptions with emojis and expanded descriptions
-st.header("Trait Descriptions")
-for trait, description in trait_descriptions.items():
-    st.write(f"**{trait}** : {description}")
+    # Dropdown menu to select team member
+    selected_team_member = st.selectbox("Select Team Member:", df["Team Members"])
+
+    # Filter data based on the selected team member
+    selected_data = df[df["Team Members"] == selected_team_member].squeeze()
+
+    # Create a bar chart using Plotly
+    fig = go.Figure()
+
+    for i, trait in enumerate(selected_data.index[1:]):
+        fig.add_trace(go.Bar(
+            x=[trait],
+            y=[selected_data[trait]],
+            name=trait,
+            marker=dict(color=f'rgba({i * 30}, 50, 190, 0.7)'),  # Adjust color
+            text=[f'{selected_data[trait]}'],
+            textposition='auto',
+        ))
+
+    fig.update_layout(
+        title=f"Personality Traits for {selected_team_member}",
+        xaxis=dict(title="Traits"),
+        yaxis=dict(title="Values"),
+        barmode='group'
+    )
+
+    st.plotly_chart(fig)
